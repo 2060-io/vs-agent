3734493d810afa17e00a2d9267328b2fb7d1c6f4
Ariel Gentile <gentilester@gmail.com>
2024-09-26 13:38:20 -0300

    feat: update chatbot demo with features for didcomm calls (#20)

--- examples/chatbot/index.ts
+++ examples/chatbot/index.ts
@@ -2,12 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import fetch from 'node-fetch'
+import { Logger } from 'tslog'
 import { helpMessage, rockyQuotes, rootContextMenu, rootMenuAsQA, welcomeMessage, worldCupPoll } from './data'
+import { randomUUID } from 'crypto'
 
-const PORT = Number(process.env.PORT || 5002)
-const SERVICE_AGENT_BASE_URL = process.env.SERVICE_AGENT_ADMIN_BASE_URL || 'http://localhost:5000'
-const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL || 'http://localhost:5002'
+import phoneCredDefData from './phone-cred-def-dev.json'
 
+const logger = new Logger()
+
+const PORT = Number(process.env.PORT || 5000)
+const SERVICE_AGENT_BASE_URL = process.env.SERVICE_AGENT_ADMIN_BASE_URL || 'http://localhost:3000/v1'
+const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL || 'http://localhost:5000'
+const VISION_SERVICE_BASE_URL = process.env.VISION_SERVICE_BASE_URL || 'https://webrtc-pymediasoup-client-demo.dev.2060.io'
+const WEBRTC_SERVER_BASE_URL = process.env.WEBRTC_SERVER_BASE_URL || 'https://dts-webrtc.dev.2060.io'
 const app = express()
 
 const staticDir = path.join(__dirname, 'public')
@@ -19,23 +26,83 @@ app.use(express.urlencoded({ extended: true }))
 
 app.set('json spaces', 2)
 
-const phoneNumberCredentialDefinitionId =
-  process.env.PHONE_CREDENTIAL_DEFINITION_ID ||
-  'did:web:pn-vs.dev.2060.io?service=anoncreds&relativeRef=/credDef/26BYVUEasEVQDkVwttUTQm33vf9JQ35CPnySRem1VwtB'
+let phoneNumberCredentialDefinitionId: string | undefined
+
+type OngoingCall = { 
+  wsUrl: string, 
+  roomId: string,
+  connectionId: string
+ }
+
+let ongoingCalls: OngoingCall[] = []
+
+export interface CredentialTypeInfo {
+  id: string
+  name: string
+  version: string
+  attributes: string[]
+  schemaId?: string
+}
+
+export const getCredentialTypes = async () => {
+  const response = await fetch(`${SERVICE_AGENT_BASE_URL}/credential-types`, {
+    method: 'GET',
+    headers: { accept: 'application/json', 'Content-Type': 'application/json' },
+  })
+
+  const types = await response.json()
+
+  if (!Array.isArray(types)) {
+    throw new Error('Invalid response from Service Agent')
+  }
+  return types.map((value) => value as CredentialTypeInfo)
+}
+
+export const importCredentialType = async (importData: Record<string, unknown>) => {
+  logger.info(`Importing credential type ${importData.id}`)
+
+  const response = await fetch(`${SERVICE_AGENT_BASE_URL}/credential-types/import`, {
+    method: 'POST',
+    body: JSON.stringify(importData),
+    headers: { accept: 'application/json', 'Content-Type': 'application/json' },
+  })
+
+  if (!response.ok) throw new Error(`Cannot import credential type: status ${response.status}`)
+
+  return (await response.json()).id
+}
 
 const server = app.listen(PORT, async () => {
-  console.log(`Dummy chatbot started on port ${PORT}`)
+  logger.info(`Demo chatbot started on port ${PORT}`)
 
-  console.log(`phoneNumberCredentialDefinitionId: ${phoneNumberCredentialDefinitionId}`)
+  // Ensure that phone credential type is created. If it does not exist, import it from
+  // JSON (Note: This is only for dev purposes. Should not be done in production)
+  const credentialTypes = await getCredentialTypes()
+
+  if (credentialTypes.length === 0) {
+    logger.info('No credential types have been found')
+  }
+
+  const phoneNumberCredentialType = credentialTypes.find(
+    (type) => type.name === 'phoneNumber' && type.version === '1.0'
+  )
+
+  try {
+    phoneNumberCredentialDefinitionId = phoneNumberCredentialType?.id ?? (await importCredentialType(phoneCredDefData))
+    logger.info(`phoneNumberCredentialDefinitionId: ${phoneNumberCredentialDefinitionId}`)
+  } catch(error) {
+    logger.error(`Could not create or retrieve phone number credential type: ${error}`)
+  }
 })
 
 export const submitMessage = async (body: unknown) => {
-  console.log(`submitMessage: ${JSON.stringify(body)}`)
-  await fetch(`${SERVICE_AGENT_BASE_URL}/message`, {
+  logger.info(`submitMessage: ${JSON.stringify(body)}`)
+  const response = await fetch(`${SERVICE_AGENT_BASE_URL}/message`, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: { 'Content-Type': 'application/json' },
   })
+  logger.info(`response: ${await response.text()}`)
 }
 
 const submitMessageReceipt = async (receivedMessage: any, messageState: 'received' | 'viewed') => {
@@ -85,7 +152,7 @@ const sendQuestion = async (options: {
 }
 
 const handleMenuSelection = async (options: { connectionId: string; item: string }) => {
-  console.log(`handleMenuSelection: ${options.item}`)
+  logger.info(`handleMenuSelection: ${options.item}`)
   const selectedItem = options.item
   const connectionId = options.connectionId
 
@@ -98,9 +165,33 @@ const handleMenuSelection = async (options: { connectionId: string; item: string
     await sendTextMessage({ connectionId, content: welcomeMessage })
   }
 
-  // Chat
-  if (selectedItem === 'chat' || selectedItem === '🗨️ New chat') {
-    if (phoneNumberCredentialDefinitionId === '') {
+  // Issue credential
+  if (selectedItem === 'issue' || selectedItem === 'Issue credential') {
+    if (!phoneNumberCredentialDefinitionId || phoneNumberCredentialDefinitionId === '') {
+      await sendTextMessage({
+        connectionId,
+        content: 'Service not available',
+      })
+    } else {
+      const body = {
+        type: 'credential-issuance',
+        connectionId,
+        credentialDefinitionId: phoneNumberCredentialDefinitionId,
+        claims: [
+          {
+            name: 'phoneNumber',
+            mimeType: 'text/plain',
+            value: '+5712345678',
+          },
+        ],
+      }
+      await submitMessage(body)
+    }
+  }
+
+  // Proof
+  if (selectedItem === 'proof' || selectedItem === 'Request proof') {
+    if (!phoneNumberCredentialDefinitionId || phoneNumberCredentialDefinitionId === '') {
       await sendTextMessage({
         connectionId,
         content: 'Service not available',
@@ -117,7 +208,8 @@ const handleMenuSelection = async (options: { connectionId: string; item: string
           {
             id: '1',
             type: 'verifiable-credential',
-            value: { credentialDefinitionId: phoneNumberCredentialDefinitionId, attributes: ['phoneNumber'] },
+            credentialDefinitionId: phoneNumberCredentialDefinitionId,
+            attributes: ['phoneNumber'],
           },
         ],
       }
@@ -133,10 +225,7 @@ const handleMenuSelection = async (options: { connectionId: string; item: string
   // Rocky quotes
   if (selectedItem === 'rocky' || selectedItem === '💪 Rocky quotes' || selectedItem === 'Inspire me!') {
     // send random Rocky quote
-    await sendTextMessage({
-      connectionId,
-      content: rockyQuotes[Math.floor(Math.random() * rockyQuotes.length)],
-    })
+    await sendTextMessage({ connectionId, content: rockyQuotes[Math.floor(Math.random() * rockyQuotes.length)] })
     await sendQuestion({
       connectionId,
       question: {
@@ -157,9 +246,9 @@ const handleMenuSelection = async (options: { connectionId: string; item: string
   }
 
   // World Cup poll responses
-  const worldCupResponses = worldCupPoll.menuItems.map(item => item.text)
+  const worldCupResponses = worldCupPoll.menuItems.map((item: { id: string; text: string }) => item.id)
   if (worldCupResponses.includes(selectedItem)) {
-    if (selectedItem === '🇦🇷 Argentina') {
+    if (selectedItem === 'argentina') {
       // Yes!
       await sendTextMessage({ connectionId, content: 'Correct! Vamos Argentina!' })
     } else {
@@ -188,7 +277,7 @@ const handleMenuSelection = async (options: { connectionId: string; item: string
 
 app.post('/connection-state-updated', async (req, res) => {
   const obj = req.body
-  console.log(`connection state updated: ${JSON.stringify(obj)}`)
+  logger.info(`connection state updated: ${JSON.stringify(obj)}`)
   if (obj.state === 'completed') {
     await sendRootMenu(obj.connectionId)
     await sendTextMessage({
@@ -201,19 +290,20 @@ app.post('/connection-state-updated', async (req, res) => {
 
 app.post('/message-state-updated', async (req, res) => {
   const obj = req.body
-  console.log(`message state updated: ${JSON.stringify(obj)}`)
+  logger.info(`message state updated: ${JSON.stringify(obj)}`)
   res.json({ message: 'ok' })
 })
 
 app.post('/message-received', async (req, res) => {
   const obj = req.body.message
-  console.log(`received message: ${JSON.stringify(obj)}`)
+  logger.info(`received message: ${JSON.stringify(obj)}`)
+  res.json({ message: 'ok' }).send()
 
   if (obj.type === 'text') {
     await submitMessageReceipt(obj, 'viewed')
     const connectionId = obj.connectionId
     const content = obj.content as string
-
+    logger.info(`Content: ${content}`)
     if (content.startsWith('/echo')) {
       await sendTextMessage({ connectionId, content: `${content.substring(5)}` })
     } else if (content.startsWith('/context')) {
@@ -221,28 +311,122 @@ app.post('/message-received', async (req, res) => {
     } else if (content.startsWith('/menu')) {
       await sendQuestion({ connectionId: obj.connectionId, question: rootMenuAsQA })
     } else if (content.startsWith('/media')) {
+      const mediaParametersRegExp = /"([^"\\]*(?:\\.[^"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'|[^\s]+/g
+      const parsedContents = content.substring(7).match(mediaParametersRegExp)
       const body = {
         type: 'media',
         connectionId: obj.connectionId,
-        description: 'An image',
-        uri: `${content.substring(7)}` || `${PUBLIC_BASE_URL}/bunny.jpeg`,
-        mimeType: 'image/jpeg', // TODO: take mime type from actual media
+        description: parsedContents?.slice(1).join(' ') ?? 'An image',
+        items: [
+          {
+            description: parsedContents?.slice(1).join(' ') ?? 'Bunny',
+            uri: parsedContents ? `${parsedContents[0]}` : `${PUBLIC_BASE_URL}/bunny.jpeg`,
+            mimeType: 'image/jpeg', // TODO: take mime type from actual media
+            filename: `${randomUUID()}.jpg`,
+            byteCount: 6576,
+          },
+        ],
       }
       await submitMessage(body)
-    } else if (content.startsWith('/proof')) {
+      // Format: /link URL (opt)title (opt)description (opt)iconUrl (opt)openingMode
+    } else if (content.startsWith('/link')) {
+      const linkParametersRegExp = /"([^"\\]*(?:\\.[^"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'|[^\s]+/g
+      const parsedContents = content.substring(6).match(linkParametersRegExp)
+
+      if (parsedContents) {
+        const body = {
+          type: 'media',
+          connectionId: obj.connectionId,
+          description: parsedContents[2],
+          items: [
+            {
+              uri: parsedContents[0] || 'https://2060.io',
+              mimeType: 'text/html',
+              title: parsedContents[1] || 'Title',
+              icon: parsedContents[3] || '',
+              openingMode: parsedContents[4] || 'normal',
+            },
+          ],
+        }
+        await submitMessage(body)
+      }
+    } else if (content.startsWith('/invitation')) {
+      const parsedContents = content.split(' ')
+      const body = {
+        type: 'invitation',
+        connectionId: obj.connectionId,
+        label: parsedContents[1],
+        imageUrl: parsedContents[2],
+        did: parsedContents[3],
+      }
+      await submitMessage(body)
+    } else if (content.startsWith('/profile')) {
+      const parsedContents = content.split(' ')
+      const body = {
+        type: 'profile',
+        connectionId: obj.connectionId,
+        displayName: parsedContents[1],
+        displayImageUrl: parsedContents[2],
+        displayIconUrl: parsedContents[3],
+      }
+      await submitMessage(body)
+    } else if (content.startsWith('/call')) {
+      const parsedContents = content.split(' ')
+      let wsUrl = parsedContents[1]
+      let roomId = parsedContents[2]
+
+      try {
+        if (!wsUrl || !roomId) {
+          // Create a room if not given as argument
+          const result = await fetch(`${WEBRTC_SERVER_BASE_URL}/rooms`, { headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },method: 'POST', body: JSON.stringify({ maxPeerCount: 2, eventNotificationUri: `${PUBLIC_BASE_URL}/call-events` }) })
+
+          const response = await result.json()
+
+          wsUrl = response.wsUrl
+          roomId = response.roomId
+        }
+
+        const peerId = obj.connectionId // We re-use connection id to simplify
+
+
+        ongoingCalls.push({ wsUrl, roomId, connectionId: req.body.peerId })
+        const body = {
+          type: 'call-offer',
+          connectionId: obj.connectionId,
+          parameters: { wsUrl, roomId, peerId }
+        }
+        await submitMessage(body)
+      } catch(reason) {
+        logger.error(`Cannot create call: ${reason}`)
+        sendTextMessage({connectionId: obj.connectionId, content: 'An error has occurred while creating a call' })
+      }
+    }
+     else if (content.startsWith('/proof')) {
       const body = {
         type: 'identity-proof-request',
-        connectionId: obj.connectionId,
-        requestedProofItems: [{ id: '1', type: 'verifiable-credential', value: {} }],
+        connectionId,
+        requestedProofItems: [
+          {
+            id: '1',
+            type: 'verifiable-credential',
+            credentialDefinitionId: phoneNumberCredentialDefinitionId,
+          },
+        ],
       }
       await submitMessage(body)
     } else if (content.startsWith('/rocky')) {
-      await sendTextMessage({
-        connectionId,
-        content: rockyQuotes[Math.floor(Math.random() * rockyQuotes.length)],
-      })
+      await sendTextMessage({ connectionId, content: rockyQuotes[Math.floor(Math.random() * rockyQuotes.length)] })
     } else if (content.startsWith('/help')) {
       await sendTextMessage({ connectionId, content: helpMessage })
+    } else if (content.startsWith('/terminate')) {
+      const body = {
+        type: 'terminate-connection',
+        connectionId,
+      }
+      await submitMessage(body)
     } else {
       // Text message received but not understood
       await sendTextMessage({
@@ -261,13 +445,61 @@ app.post('/message-received', async (req, res) => {
     await handleMenuSelection({ connectionId: obj.connectionId, item: obj.selectionId ?? 'nothing' })
   } else if (obj.type === 'identity-proof-submit') {
     await submitMessageReceipt(obj, 'viewed')
-    await sendTextMessage({
-      connectionId: obj.connectionId,
-      content: 'We have successfully received your proof submission. Enjoy the service!',
-    })
+    const errorCode = obj.submittedProofItems[0].errorCode
+    if (errorCode) {
+      await sendTextMessage({ connectionId: obj.connectionId, content: `Error code received: ${errorCode}` })
+    } else {
+      await sendTextMessage({
+        connectionId: obj.connectionId,
+        content: 'We have successfully received your proof submission. Enjoy the service!',
+      })
+    }
+  } else if (obj.type === 'media') {
+    logger.info('media received')
+    await submitMessageReceipt(obj, 'viewed')
+  }
+})
+
+app.post('/call-events', async (req, res) => {
+  logger.info(`Event received: ${JSON.stringify(req.body)}`)
+  const event = req.body.event
+  // find call
+  const call = ongoingCalls.find(item => item.connectionId === req.body.peerId)
+
+  if (!call) {
+    logger.warn('No call matching the event')
+    return
   }
 
-  res.json({ message: 'ok' })
+  if (event === 'peer-joined') {
+        // Ask Vision service to join the call
+        await fetch(`${VISION_SERVICE_BASE_URL}/join-call`, { 
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'POST', 
+          body: JSON.stringify({ 
+              ws_url: call.wsUrl, 
+              success_url: `${PUBLIC_BASE_URL}/call-success/${call.connectionId}`, 
+              failure_url: `${PUBLIC_BASE_URL}/call-failure/${call.connectionId}` 
+            }) 
+        })
+  } else if (event === 'peer-left') {
+    ongoingCalls.splice(ongoingCalls.indexOf(call), 1)
+  }
+})
+
+app.put('/call-success/:connectionId', (req, res) => {
+  logger.info(`Call success for ${req.params.connectionId}: ${JSON.stringify(req.body)}`)
+  res.end()
+  sendTextMessage({ connectionId: req.params.connectionId, content: 'Call was succesful'}).catch(error => logger.error(`Cannot send message: ${error}`))
+})
+
+app.put('/call-failure/:connectionId', (req, res) => {
+  logger.info(`Call success for ${req.params.connectionId}: ${JSON.stringify(req.body)}`)
+  res.end()
+  sendTextMessage({ connectionId: req.params.connectionId, content: 'Call failed'}).catch(error => logger.error(`Cannot send message: ${error}`))
 })
 
 export { app, server }
