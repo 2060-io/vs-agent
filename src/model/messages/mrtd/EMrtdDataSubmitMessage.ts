404db4ffbfb6afc9af98f6b7ca44fbf680b288ee
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-22 16:29:40 -0500

    fix: lint

--- src/model/messages/mrtd/EMrtdDataSubmitMessage.ts
+++ src/model/messages/mrtd/EMrtdDataSubmitMessage.ts
@@ -5,22 +5,22 @@ import { BaseMessage, BaseMessageOptions } from '../BaseMessage'
 import { MessageType } from '../MessageType'
 
 export type EMrtdRawData = {
-  raw: Record<string, string>;
+  raw: Record<string, string>
   processed: {
-      fields?: EMrtdProcessedData;
-  };
-};
+    fields?: EMrtdProcessedData
+  }
+}
 
 export type EMrtdProcessedData = {
-  mrzData: string;
-  firstName: string;
-  lastName: string;
-  faceDataUrl: string;
-  fingerprintDataUrl?: string;
-  birthDate: number;
-  placeOfBirth: string;
-  issuanceDate: number;
-};
+  mrzData: string
+  firstName: string
+  lastName: string
+  faceDataUrl: string
+  fingerprintDataUrl?: string
+  birthDate: number
+  placeOfBirth: string
+  issuanceDate: number
+}
 
 export interface EMrtdDataSubmitMessageOptions extends BaseMessageOptions {
   dataGroups: EMrtdData
@@ -46,25 +46,25 @@ export class EMrtdDataSubmitMessage extends BaseMessage {
   public dataGroups!: EMrtdRawData
 
   public parseDataGroups({ raw, parsed }: EMrtdData): EMrtdRawData {
-    if (!parsed || !parsed.fields) return this.dataGroups;
+    if (!parsed || !parsed.fields) return this.dataGroups
 
-    const [lastName, firstName] = parsed.fields.additionalPersonalData?.nameOfHolder.split('<<') || [];
-    const faceMimeType = parsed.fields.images[0].imageType === 1 ? 'image/jp2':'image/jpeg'
+    const [lastName, firstName] = parsed.fields.additionalPersonalData?.nameOfHolder.split('<<') || []
+    const faceMimeType = parsed.fields.images[0].imageType === 1 ? 'image/jp2' : 'image/jpeg'
 
     const newEmrtdData: EMrtdRawData = {
       raw: raw,
       processed: {
         fields: {
-            mrzData: parsed.fields.mrzData ?? 'null',
-            firstName: firstName ? firstName.replace('<', ' ').trim() : 'null',
-            lastName: lastName ? lastName.replace('<', ' ').trim() : 'null',
-            faceDataUrl: `data:${faceMimeType};base64,${(parsed.fields.images[0].imageData).toString('base64')}`,
-            birthDate: parsed.fields.additionalPersonalData?.fullDateOfBirth ?? 0,
-            placeOfBirth: parsed.fields.additionalPersonalData?.placeOfBirth[0] ?? "null",
-            issuanceDate:parsed.fields.additionalDocumentData?.dateOfIssue ?? 0,
-        }
-      }
+          mrzData: parsed.fields.mrzData ?? 'null',
+          firstName: firstName ? firstName.replace('<', ' ').trim() : 'null',
+          lastName: lastName ? lastName.replace('<', ' ').trim() : 'null',
+          faceDataUrl: `data:${faceMimeType};base64,${parsed.fields.images[0].imageData.toString('base64')}`,
+          birthDate: parsed.fields.additionalPersonalData?.fullDateOfBirth ?? 0,
+          placeOfBirth: parsed.fields.additionalPersonalData?.placeOfBirth[0] ?? 'null',
+          issuanceDate: parsed.fields.additionalDocumentData?.dateOfIssue ?? 0,
+        },
+      },
     }
-    return newEmrtdData;
+    return newEmrtdData
   }
 }
