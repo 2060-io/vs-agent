97267cfa61f7e7ba0bef650651719c2f7251157b
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-23 10:33:29 -0500

    fix: new structure

--- src/model/messages/mrtd/EMrtdDataSubmitMessage.ts
+++ src/model/messages/mrtd/EMrtdDataSubmitMessage.ts
@@ -8,42 +8,28 @@ import { MessageType } from '../MessageType'
 export type EMrtdRawData = {
   raw: Record<string, string>
   processed: {
-    ef_dg1: EF_DG1
-    ef_dg2: EF_DG2
-    ef_dg11?: EF_DG11
+    documentType: string
+    documentNumber?: string
+    issuingState?: string
+    dateOfBirth?: string
+    dateOfExpiry?: string
+    sex?: string
+    nationality?: string
+    lastName?: string
+    firstName?: string
+    mrzOptionalData?: string
+    faceImages: string[]
+    nameOfHolder?: string
+    otherNames?: string[]
+    personalNumber?: string
+    placeOfBirth?: string[]
+    permanentAddress?: string[]
+    telephone?: string
+    profession?: string
+    title?: string
+    personalSummary?: string
+    custodyInformation?: string
   }
-}
-
-export type EF_DG1 = {
-  documentType: string
-  documentNumber: string | null
-  issuingState?: string | null
-  dateOfBirth?: string | null
-  dateOfExpiry?: string | null
-  sex?: string | null
-  nationality?: string | null
-  lastName?: string | null
-  firstName?: string | null
-  nameOfHolder?: string
-  mrzOptionalData?: string | null
-}
-
-export type EF_DG2 = {
-  faceImages: string[]
-}
-
-export type EF_DG11 = {
-  nameOfHolder?: string
-  dateOfBirth?: number
-  otherNames?: string[]
-  personalNumber?: string
-  placeOfBirth?: string[]
-  permanentAddress?: string[]
-  telephone?: string
-  profession?: string
-  title?: string
-  personalSummary?: string
-  custodyInformation?: string
 }
 
 export interface EMrtdDataSubmitMessageOptions extends BaseMessageOptions {
@@ -99,35 +85,32 @@ export class EMrtdDataSubmitMessage extends BaseMessage {
     const newEmrtdData: EMrtdRawData = {
       raw: raw,
       processed: {
-        ef_dg1: {
-          documentType: parsedMrz.format,
-          documentNumber: parsedMrz.documentNumber,
-          issuingState: parsedMrz.fields.issuingState,
-          dateOfBirth: parsedMrz.fields.birthDate,
-          dateOfExpiry: parsedMrz.fields.expirationDate,
-          sex: parsedMrz.fields.sex,
-          nationality: parsedMrz.fields.nationality,
-          lastName: parsedMrz.fields.lastName,
-          firstName: parsedMrz.fields.firstName,
-          nameOfHolder: parsed.fields.additionalPersonalData?.nameOfHolder,
-          mrzOptionalData: parsedMrz.fields.optional1,
-        },
-        ef_dg2: {
-          faceImages: dataUrls,
-        },
-        ef_dg11: {
-          nameOfHolder: parsed.fields.additionalPersonalData?.nameOfHolder,
-          dateOfBirth: parsed.fields.additionalPersonalData?.fullDateOfBirth,
-          otherNames: parsed.fields.additionalPersonalData?.otherNames,
-          personalNumber: parsed.fields.additionalPersonalData?.personalNumber,
-          placeOfBirth: parsed.fields.additionalPersonalData?.placeOfBirth,
-          permanentAddress: parsed.fields.additionalPersonalData?.permanentAddress,
-          telephone: parsed.fields.additionalPersonalData?.telephone,
-          profession: parsed.fields.additionalPersonalData?.profession,
-          title: parsed.fields.additionalPersonalData?.title,
-          personalSummary: parsed.fields.additionalPersonalData?.personalSummary,
-          custodyInformation: parsed.fields.additionalPersonalData?.custodyInformation,
-        },
+        documentType: parsedMrz.format,
+        documentNumber: parsedMrz.documentNumber ?? undefined,
+        issuingState: parsedMrz.fields.issuingState ?? undefined,
+        dateOfExpiry: parsedMrz.fields.expirationDate ?? undefined, // TODO: Check and specify date format
+        sex: parsedMrz.fields.sex ?? undefined,
+        nationality: parsedMrz.fields.nationality ?? undefined,
+        lastName: parsedMrz.fields.lastName ?? undefined,
+        firstName: parsedMrz.fields.firstName ?? undefined,
+        mrzOptionalData: parsedMrz.fields.optional1 ?? undefined,
+        faceImages: dataUrls,
+        nameOfHolder:
+          parsed.fields.additionalPersonalData?.nameOfHolder ??
+          `${parsedMrz.fields.lastName} ${parsedMrz.fields.firstName}`,
+        dateOfBirth:
+          parsed.fields.additionalPersonalData?.fullDateOfBirth.toString() ??
+          parsedMrz.fields.birthDate ??
+          undefined, // TODO: Check and specify date format
+        otherNames: parsed.fields.additionalPersonalData?.otherNames,
+        personalNumber: parsed.fields.additionalPersonalData?.personalNumber,
+        placeOfBirth: parsed.fields.additionalPersonalData?.placeOfBirth,
+        permanentAddress: parsed.fields.additionalPersonalData?.permanentAddress,
+        telephone: parsed.fields.additionalPersonalData?.telephone,
+        profession: parsed.fields.additionalPersonalData?.profession,
+        title: parsed.fields.additionalPersonalData?.title,
+        personalSummary: parsed.fields.additionalPersonalData?.personalSummary,
+        custodyInformation: parsed.fields.additionalPersonalData?.custodyInformation,
       },
     }
     return newEmrtdData
