b29b42bfc168f9a622c58767f906666fc5d90a5e
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-11-15 14:08:20 -0500

    fat: upload client https utils

---
+++ packages/client/src/utils/HttpUtils.ts
@@ -0,0 +1,23 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+
+export class HttpUtils {
+  static handleException(
+    logger: Logger,
+    error: any,
+    defaultMessage: string = 'Internal server error'
+  ): never {
+    logger.error(`Error: ${error.stack}`);
+
+    throw new HttpException(
+      {
+        statusCode: error.statusCode ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        error: error.message ?? defaultMessage,
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      {
+        cause: error,
+      },
+    );
+  }
+}
