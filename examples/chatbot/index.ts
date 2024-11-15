3383f37106ac54feb41de561a6a39c9e436a8309
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-11-15 11:41:21 -0500

    fix: adjust

--- examples/chatbot/index.ts
+++ examples/chatbot/index.ts
@@ -5,7 +5,6 @@ import {
   CredentialIssuanceMessage,
   EMrtdDataRequestMessage,
   EMrtdDataSubmitMessage,
-  EventType,
   IdentityProofRequestMessage,
   IdentityProofSubmitMessage,
   InvitationMessage,
