ddfdbe68de6bc03cc3c910dddfcb03b9af3ea12c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-01-22 17:29:56 -0500

    fix: merge

--- packages/nestjs-client/src/app.module.ts
+++ packages/nestjs-client/src/app.module.ts
@@ -7,9 +7,6 @@ import { MessageEventModule } from './messages'
 import { EventsModuleOptions } from './types'
 
 @Global()
-import { EventsModuleOptions } from './types'
-
-@Global()
 @Module({})
 export class EventsModule {
   static register(options: EventsModuleOptions): DynamicModule {
