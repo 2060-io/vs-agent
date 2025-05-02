881ddbf89142c20cdeaac0d5c3dafcde8b0fe912
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-05-02 11:18:42 -0500

    test: add well-known/did.jsonl
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- packages/main/src/didWebServer.ts
+++ packages/main/src/didWebServer.ts
@@ -14,6 +14,8 @@ import fs from 'fs'
 import multer, { diskStorage } from 'multer'
 
 import { ServiceAgent } from './utils/ServiceAgent'
+import { type AgentContext, Buffer, Key, KeyType } from '@credo-ts/core'
+import { AbstractCrypto, resolveDID, type SigningOutput } from 'didwebvh-ts'
 
 export const startDidWebServer = async (agent: ServiceAgent, config: DidWebServerConfig) => {
   const app = config.app ?? express()
@@ -82,6 +84,21 @@ export const addDidWebRoutes = async (
       const didDocument = didRecord.didDocument
       if (didDocument) {
         res.send(didDocument.toJSON())
+      } else {
+        res.status(500).end()
+      }
+    } else {
+      res.status(404).end()
+    }
+  })
+
+  app.get('/.well-known/did.jsonl', async (req, res) => {
+    agent.config.logger.info(`Public DidDocument requested`)
+    if (agent.did) {
+      const crypto = new DIDWebvhCrypto(agent.context)
+      const { doc: didDocument } = await resolveDID( agent.did, { verifier: crypto })
+      if (didDocument) {
+        res.send(didDocument)
       } else {
         res.status(500).end()
       }
@@ -273,5 +290,48 @@ export const addDidWebRoutes = async (
         res.status(200).end()
       },
     )
+  }
+}
+
+export class DIDWebvhCrypto extends AbstractCrypto {
+  private agentContext: AgentContext
+
+  public constructor(agentContext: AgentContext) {
+    super({
+      verificationMethod: {
+        id: 'did:webvh:123',
+        controller: 'did:webvh:123',
+        type: 'Ed25519VerificationKey2020',
+        publicKeyMultibase: '123',
+        secretKeyMultibase: '123',
+      },
+    })
+    this.agentContext = agentContext
+  }
+
+  public async sign(): Promise<SigningOutput> {
+    throw new Error('Not implemented')
+  }
+
+  public async verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array): Promise<boolean> {
+    try {
+      if (!this.agentContext) {
+        throw new Error('Agent context is required')
+      }
+
+      const key = new Key(publicKey, KeyType.Ed25519)
+
+      return await this.agentContext.wallet.verify({
+        key,
+        data: Buffer.from(message),
+        signature: Buffer.from(signature),
+      })
+    } catch (error) {
+      // Log error in a non-production environment
+      if (process.env.NODE_ENV !== 'production') {
+        this.agentContext.config.logger.error('Error verifying signature:', error)
+      }
+      return false
+    }
   }
 }
