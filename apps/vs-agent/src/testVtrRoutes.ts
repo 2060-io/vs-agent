a56c2c7aa7096970bbedba5dd0652cde52649fe5
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-07-17 17:30:54 -0500

    fix: use base image
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- apps/vs-agent/src/testVtrRoutes.ts
+++ apps/vs-agent/src/testVtrRoutes.ts
@@ -377,15 +377,14 @@ export const addTestVtrRoutes = async (app: express.Express, agent: VsAgent, pub
  * @returns A Base64 data URI string, or a fallback placeholder if the image cannot be fetched or is invalid.
  */
 export async function urlToBase64(url?: string): Promise<string> {
-  const FALLBACK_BASE64 = 'base64string'
+  const FALLBACK_BASE64 = 'https://hologram.zone/images/ico-hologram.png'
 
   if (!url) {
     console.warn('No URL provided for image conversion.')
-    return FALLBACK_BASE64
   }
 
   try {
-    const response = await axios.get(url, { responseType: 'arraybuffer' })
+    const response = await axios.get(url ?? FALLBACK_BASE64, { responseType: 'arraybuffer' })
 
     const contentType = response.headers['content-type']
     if (!contentType || !contentType.startsWith('image/')) {
