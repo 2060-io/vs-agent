8a87adb656047abe0cb9e8db31659228e183bbb9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-09-10 16:41:49 -0500

    feat: add create proof for register methods on webvh
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- apps/vs-agent/src/utils/DidWebVhAnonCredsRegistrar.ts
+++ apps/vs-agent/src/utils/DidWebVhAnonCredsRegistrar.ts
@@ -1,19 +1,29 @@
-import { RegisterCredentialDefinitionOptions, RegisterCredentialDefinitionReturn, RegisterRevocationRegistryDefinitionOptions, RegisterRevocationRegistryDefinitionReturn, RegisterRevocationStatusListOptions, RegisterRevocationStatusListReturn, RegisterSchemaOptions, RegisterSchemaReturn } from "@credo-ts/anoncreds";
-import { AgentContext, CredoError, TypedArrayEncoder } from "@credo-ts/core";
-import { MultiBaseEncoder, MultiHashEncoder } from "@credo-ts/core/build/utils";
-import { WebVhAnonCredsRegistry } from "@credo-ts/webvh";
+import {
+  RegisterCredentialDefinitionOptions,
+  RegisterCredentialDefinitionReturn,
+  RegisterRevocationRegistryDefinitionOptions,
+  RegisterRevocationRegistryDefinitionReturn,
+  RegisterRevocationStatusListOptions,
+  RegisterRevocationStatusListReturn,
+  RegisterSchemaOptions,
+  RegisterSchemaReturn,
+} from '@credo-ts/anoncreds'
+import { AgentContext, CredoError, TypedArrayEncoder } from '@credo-ts/core'
+import { MultiBaseEncoder, MultiHashEncoder } from '@credo-ts/core/build/utils'
+import { WebVhAnonCredsRegistry } from '@credo-ts/webvh'
 import { canonicalize } from 'json-canonicalize'
+import { EddsaJcs2022Cryptosuite, unsecuredDocument } from './eddsa-jcs-2022'
 
 export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
-
-  constructor(){ super() }
-
-  public async registerSchema(agentContext: AgentContext, options?: RegisterSchemaOptions): Promise<RegisterSchemaReturn> {
+  public async registerSchema(
+    agentContext: AgentContext,
+    options?: RegisterSchemaOptions,
+  ): Promise<RegisterSchemaReturn> {
     if (!options?.schema) throw new CredoError('Schema options must be provided.')
-    
+
     const resourceId = this.digestMultibase(canonicalize(options.schema))
 
-    const schemaId = `${options.schema.issuerId}/resources/${resourceId}`
+    const schemaId = `${options.schema.issuerId}/anoncreds/v1/schema/${resourceId}`
     return {
       schemaState: { state: 'finished', schema: options.schema, schemaId },
       registrationMetadata: {},
@@ -21,12 +31,15 @@ export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
     }
   }
 
-  public async registerCredentialDefinition(agentContext: AgentContext, options?: RegisterCredentialDefinitionOptions): Promise<RegisterCredentialDefinitionReturn> {
+  public async registerCredentialDefinition(
+    agentContext: AgentContext,
+    options?: RegisterCredentialDefinitionOptions,
+  ): Promise<RegisterCredentialDefinitionReturn> {
     if (!options?.credentialDefinition) throw new CredoError('credentialDefinition options must be provided.')
 
     const resourceId = this.digestMultibase(canonicalize(options.credentialDefinition))
 
-    const credentialDefinitionId = `${options.credentialDefinition.issuerId}/resources/${resourceId}`
+    const credentialDefinitionId = `${options.credentialDefinition.issuerId}/anoncreds/v1/credDef/${resourceId}`
 
     return {
       credentialDefinitionState: {
@@ -41,14 +54,15 @@ export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
 
   public async registerRevocationRegistryDefinition(
     agentContext: AgentContext,
-    options?: RegisterRevocationRegistryDefinitionOptions
+    options?: RegisterRevocationRegistryDefinitionOptions,
   ): Promise<RegisterRevocationRegistryDefinitionReturn> {
-    if (!options?.revocationRegistryDefinition) throw new CredoError('credentialDefinition options must be provided.')
+    if (!options?.revocationRegistryDefinition)
+      throw new CredoError('credentialDefinition options must be provided.')
 
     // Nothing to actually do other than generating a revocation registry definition id
     const resourceId = this.digestMultibase(canonicalize(options.revocationRegistryDefinition))
 
-    const revocationRegistryDefinitionId = `${options.revocationRegistryDefinition.issuerId}/resources/${resourceId}`
+    const revocationRegistryDefinitionId = `${options.revocationRegistryDefinition.issuerId}/anoncreds/v1/revRegDef/${resourceId}`
 
     return {
       revocationRegistryDefinitionState: {
@@ -61,7 +75,10 @@ export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
     }
   }
 
-  public async registerRevocationStatusList(agentContext: AgentContext, options?: RegisterRevocationStatusListOptions): Promise<RegisterRevocationStatusListReturn> {
+  public async registerRevocationStatusList(
+    agentContext: AgentContext,
+    options?: RegisterRevocationStatusListOptions,
+  ): Promise<RegisterRevocationStatusListReturn> {
     if (!options?.revocationStatusList) throw new CredoError('revocationStatusList options must be provided.')
 
     // Nothing to actually do other than adding a timestamp
@@ -69,7 +86,7 @@ export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
     const latestRevocationStatusList = await this.getRevocationStatusList(
       agentContext,
       options.revocationStatusList.revRegDefId,
-      timestamp
+      timestamp,
     )
 
     return {
@@ -84,11 +101,33 @@ export class DidWebVhAnonCredsRegistrar extends WebVhAnonCredsRegistry {
       },
     }
   }
-  
+
   private digestMultibase(value: string) {
     const valueBytes = TypedArrayEncoder.fromString(value)
     const digestMultihash = MultiHashEncoder.encode(valueBytes, 'sha-256')
     const digestMultibase = MultiBaseEncoder.encode(digestMultihash, 'base58btc')
     return digestMultibase
+  }
+
+  public async createProof(agentContext: AgentContext, unsecuredDocument: unsecuredDocument, verificationMethod: string) {
+    const cryptosuite = new EddsaJcs2022Cryptosuite(agentContext)
+    try {
+      const creationResult = await cryptosuite.createProof(
+        unsecuredDocument,
+        {
+          type: 'DataIntegrityProof',
+          cryptosuite: 'eddsa-jcs-2022',
+          verificationMethod,
+          proofPurpose: 'assertionMethod',
+        }
+      )
+      return creationResult
+    } catch (error) {
+      agentContext.config.logger.error('Error during proof creation of did:webvh resource', {
+        error: error instanceof Error ? error.message : String(error),
+        stack: error instanceof Error ? error.stack : undefined,
+      })
+      return null
+    }
   }
 }
