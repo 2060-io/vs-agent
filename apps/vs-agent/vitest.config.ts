e72d93b8e8ea67ee3e7cb03039c2cf3772302327
andres vallecilla <andresfelipe083195@hotmail.com>
2025-12-03 16:08:04 -0500

    test: update config

---
+++ apps/vs-agent/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import rootConfig from '../../vitest.config'
+import { existsSync } from 'fs'
+
+const setup = 'tests/__mocks__/global-setup.ts'
+export default mergeConfig(
+  rootConfig,
+  defineConfig({
+    test: {
+      setupFiles: existsSync(setup) ? [setup] : [],
+      include: ['tests/**/*.test.ts'],
+      reporters: ['verbose'],
+      outputFile: './test-results.json'
+    }
+  })
+)
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,11 @@
 import { existsSync } from "fs";
 import { defineConfig } from "vitest/config";
 
-const setup = 'tests/__mocks__/global-setup.ts'
 export default defineConfig({
   test: {
     environment: "node",
     passWithNoTests: true,
     clearMocks: true,
-    setupFiles: existsSync(setup) ? [setup] : [],
     coverage: {
       provider: "v8",
       reportsDirectory: "./coverage",
